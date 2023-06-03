10d13d9b600f2dfb9886c92842119608444b135e
aidant <15520814+aidant@users.noreply.github.com>
2023-06-03 14:24:43 +1000

    disable ui

--- packages/app/src/index.svelte
+++ packages/app/src/index.svelte
@@ -1,11 +1,5 @@
-<script lang="ts">
-  import Analytics from './components/analytics.svelte'
-  import Header from './components/header.svelte'
-  import { token } from './store'
-
-</script>
-
-<Header />
-{#if $token}
-  <Analytics />
-{/if}
+<div class='h-full w-full flex flex-col justify-center items-center'>
+  <p class='max-w-prose m-12'>
+    Ragnarok Analytics has been sunset, if you need to access please contact me.
+  </p>
+</div>
